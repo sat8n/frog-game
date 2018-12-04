1e89129a03913752fd270acf57013d6a7a8fbe25
J (Nabilah Abd Malek) <na17967@essex.ac.uk>
2018-12-04 11:49:44 +0000

    Added the foundation of the application

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GameFrame {
+    JFrame frame;
+    JButton start_game, settings, highscore, backtoHome;
+    JLabel gameName;
+    JPanel startScreen, gamePage, settingsPage, infoPage;
+
+    public GameFrame() {
+        frame = new JFrame("Game 1705063");
+        frame.getContentPane().setBackground(new Color(255,233,137));
+
+        frame.setPreferredSize(new Dimension(600,600));
+        frame.setResizable(false);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
 }
