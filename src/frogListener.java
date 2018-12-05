ad3b813479087ddae4a442e58c518356d7f16a74
sat8n <na17967@essex.ac.uk>
2018-12-05 11:27:00 +0000

    Created a separate class for frogListener

---
+++ src/frogListener.java
@@ -0,0 +1,39 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+class frogListener implements KeyListener {
+    GameFrame theFrame;
+
+    public frogListener(GameFrame f) {
+        this.theFrame = f;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (theFrame.posx > 50) {
+                theFrame.posx -= 5;
+            }
+            theFrame.gamePage.repaint();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (theFrame.posx < 500) {
+                theFrame.posx += 5;
+            }
+            theFrame.gamePage.repaint();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            if (theFrame.posy > 80) {
+                theFrame.posy -= 7;
+            }
+            theFrame.gamePage.repaint();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            if (theFrame.posy < 481) {
+                theFrame.posy += 7;
+            }
+            theFrame.gamePage.repaint();
+        }
+    }
+    public void keyTyped(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) { }
+}
