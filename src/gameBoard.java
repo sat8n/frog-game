13d72b66767dbc1e40dedb8eff8cdd9a0e95cc43
sat8n <44139594+sat8n@users.noreply.github.com>
2018-12-07 22:49:42 +0000

    Paints 5 flies on the board that constantly change position

--- src/gameBoard.java
+++ src/gameBoard.java
@@ -1,14 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class gameBoard extends JPanel {
-    final int width = 55, height = 35;
+    private final int width = 55, height = 35;
     int posx = (((378-323)/2) + 323) - (width/2);
     int posy = (((604-556)/2) + 556) - (height/2);
-    int centerX, centerY;
-    ArrayList<Integer> calcX, calcY; // the first set of x,y coordinates have a size of 114
-    ArrayList<int[]> frogPosition;
+    private int[] startX = {80,105,120,105,80,65};
+    private int[] startY = {71,71,95,119,119,95};
+    private final int flyWidth = 10, flyHeight = 13;
+    private int centerX, centerY, flyX, flyY;
+    private ArrayList<Integer> calcX, calcY, flyCalcX, flyCalcY; // the first set of x,y coordinates have a size of 114
+    ArrayList<int[]> frogPosition, flyPosition;
+    Random r = new Random();
 
     public gameBoard() {
        setBackground(new Color(220,221,255));
@@ -30,15 +35,16 @@ public class gameBoard extends JPanel {
         frog(g);
     }
 
-    public void DrawBoard(Graphics g) {
+    private void DrawBoard(Graphics g) {
         final int width = 55, height = 35;
-        int[] startX = {80,105,120,105,80,65};
-        int[] startY = {71,71,95,119,119,95};
         int[] x = {80,105,120,105,80,65};
         int[] y = {71,71,95,119,119,95};
         calcX = new ArrayList();
         calcY = new ArrayList();
+        flyCalcX = new ArrayList();
+        flyCalcY = new ArrayList();
         frogPosition = new ArrayList<>();
+        flyPosition = new ArrayList<>();
 
         g.setColor(new Color(255,165,0));
 
@@ -57,6 +63,9 @@ public class gameBoard extends JPanel {
                 centerX = (((x[2]-x[5])/2) + x[5])-(width/2);
                 centerY = (((y[4] - y[0])/2) + y[0])-(height/2);
                 frogPosition.add(getXY());
+                flyX = (((x[2]-x[5])/2) + x[5])-(flyWidth/2);
+                flyY = (((y[4] - y[0])/2) + y[0])-(flyHeight/2);
+                flyPosition.add(getFlyXY());
                 g.drawPolygon(x,y,6); // draws the first hexagon
                 while (count < 6) {
                     for (int i = 0; i < 6; i++) {
@@ -65,6 +74,9 @@ public class gameBoard extends JPanel {
                     centerX = (((x[2]-x[5])/2) + x[5])-(width/2);
                     centerY = (((y[4] - y[0])/2) + y[0])-(height/2);
                     frogPosition.add(getXY());
+                    flyX = (((x[2]-x[5])/2) + x[5])-(flyWidth/2);
+                    flyY = (((y[4] - y[0])/2) + y[0])-(flyHeight/2);
+                    flyPosition.add(getFlyXY());
                     g.drawPolygon(x,y,6);
                     count++;
                 }
@@ -96,6 +108,9 @@ public class gameBoard extends JPanel {
                 centerX = (((x[2]-x[5])/2) + x[5])-(width/2);
                 centerY = (((y[4] - y[0])/2) + y[0])-(height/2);
                 frogPosition.add(getXY());
+                flyX = (((x[2]-x[5])/2) + x[5])-(flyWidth/2);
+                flyY = (((y[4] - y[0])/2) + y[0])-(flyHeight/2);
+                flyPosition.add(getFlyXY());
                 g.drawPolygon(x,y,6);
                 while (count < 5) {
                     for (int i = 0; i < 6; i++) {
@@ -104,6 +119,9 @@ public class gameBoard extends JPanel {
                     centerX = (((x[2]-x[5])/2) + x[5])-(width/2);
                     centerY = (((y[4] - y[0])/2) + y[0])-(height/2);
                     frogPosition.add(getXY());
+                    flyX = (((x[2]-x[5])/2) + x[5])-(flyWidth/2);
+                    flyY = (((y[4] - y[0])/2) + y[0])-(flyHeight/2);
+                    flyPosition.add(getFlyXY());
                     g.drawPolygon(x,y,6);
                     count++;
                 }
@@ -129,6 +147,9 @@ public class gameBoard extends JPanel {
             centerX = (((x[2]-x[5])/2) + x[5])-(width/2);
             centerY = (((y[4] - y[0])/2) + y[0])-(height/2);
             frogPosition.add(getXY());
+            flyX = (((x[2]-x[5])/2) + x[5])-(flyWidth/2);
+            flyY = (((y[4] - y[0])/2) + y[0])-(flyHeight/2);
+            flyPosition.add(getFlyXY());
             g.drawPolygon(x,y,6);
             count++;
         }
@@ -146,24 +167,41 @@ public class gameBoard extends JPanel {
         centerX = (((x[2]-x[5])/2) + x[5])-(width/2);
         centerY = (((y[4] - y[0])/2) + y[0])-(height/2);
         frogPosition.add(getXY());
+        flyX = (((x[2]-x[5])/2) + x[5])-(flyWidth/2);
+        flyY = (((y[4] - y[0])/2) + y[0])-(flyHeight/2);
+        flyPosition.add(getFlyXY());
         g.drawPolygon(x,y,6);
+
+        // bzz bzz
+        g.setColor(new Color(64,64,64));
+        for (int i = 0; i < 5; i++) {
+            int randomFly = r.nextInt(114);
+            g.fillOval(flyPosition.get(randomFly)[0], flyPosition.get(randomFly)[1], flyWidth, flyHeight);
+        }
 
         // these are all the positions the frog can go to
         // the first 63 are the odd rows
         for (int i = 0; i < 114; i++) {
-            //System.out.println("frogPos: " + Arrays.toString(frogPosition.get(i)));
+            //System.out.println("fly: " + Arrays.toString(flyPosition.get(i)));
         }
     }
 
-    public void frog(Graphics g) {
+    private void frog(Graphics g) {
         g.setColor(new Color(52,169,95));
         g.fillOval(posx, posy, width, height);
     }
 
-    public int[] getXY() {
+    private int[] getXY() {
         calcX.add(centerX);
         calcY.add(centerY);
         int[] position = {calcX.get(calcX.size()-1),calcY.get(calcY.size()-1)};
+        return position;
+    }
+
+    private int[] getFlyXY() {
+        flyCalcX.add(flyX);
+        flyCalcY.add(flyY);
+        int[] position = {flyCalcX.get(flyCalcX.size()-1), flyCalcY.get(calcY.size()-1)};
         return position;
     }
 }
