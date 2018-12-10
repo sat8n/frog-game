e558fc66bb6684405c7e705b205392560dbde04c
sat8n <44139594+sat8n@users.noreply.github.com>
2018-12-10 01:37:55 +0000

    Creating frog's features

--- src/Frog.java
+++ src/Frog.java
@@ -12,7 +12,24 @@ public class Frog extends Shapes {
         this.initialY = (((604-556)/2) + 556) - (this.height/2);
     }
     public void paintComponent(Graphics g) {
+        // frog's tongue
+        g.setColor(Color.pink);
+        g.fillRect((this.posx+25), (this.posy-10), 5, 13);
+        g.fillOval((this.posx+22), (this.posy-19), 10,10);
+        // frog's body
         g.setColor(new Color(0,128,64));
         g.fillOval(this.posx, this.posy, this.width, this.height);
+        // frog's mouth
+        g.setColor(new Color(112,180,44));
+        g.fillArc(this.posx, this.posy, this.width, 30, 43,90);
+        // frog's weird seedy eyes
+        g.setColor(Color.white);
+        g.fillOval((this.posx+7), this.posy,14,14);
+        g.fillOval((this.posx+35), this.posy,14,14);
+        g.setColor(Color.black);
+        g.drawOval((this.posx+7), this.posy,14,14);
+        g.drawOval((this.posx+35), this.posy,14,14);
+        g.fillOval((this.posx+9), this.posy,7,7);
+        g.fillOval((this.posx+40), this.posy,7,7);
     }
 }
